696afc65429d5c7ec050e66f31dfb48eccaaadeb
Новиков Максим <maxom.nowik@yandex.ru>
2022-11-15 21:59:32 +0400

    add docstring,typehints

--- main_with_oop.py
+++ main_with_oop.py
@@ -1,20 +1,21 @@
 """Суперкрутая игра с заставкой."""
 
 from random import randint
-from typing import Optional
+from typing import Optional, Tuple
 
 
-DEFAULT_ATTACK = 5
-DEFAULT_DEFENCE = 10
-DEFAULT_STAMINA = 80
+DEFAULT_ATTACK: int = 5
+DEFAULT_DEFENCE: int = 10
+DEFAULT_STAMINA: int = 80
 
 
 class Character:
-    RANGE_VALUE_ATTACK = (1, 3)
-    RANGE_VALUE_DEFENCE = (1, 5)
-    SPECIAL_SKILL = 'Удача'
-    SPECIAL_BUFF = 15
-    BRIEF_DESC_CHAR_CLASS = 'отважный любитель приключений'
+    """Базовая модель персонажа."""
+    RANGE_VALUE_ATTACK: Tuple[int, int] = (1, 3)
+    RANGE_VALUE_DEFENCE: Tuple[int, int] = (1, 5)
+    SPECIAL_SKILL: str = 'Удача'
+    SPECIAL_BUFF: int = 15
+    BRIEF_DESC_CHAR_CLASS: str = 'отважный любитель приключений'
 
     def __init__(self, name) -> None:
         self.name = name
@@ -39,38 +40,46 @@ class Character:
 
 
 class Warrior(Character):
-    RANGE_VALUE_ATTACK = (3, 5)
-    RANGE_VALUE_DEFENCE = (5, 10)
-    SPECIAL_SKILL = 'Выносливость'
-    SPECIAL_BUFF = DEFAULT_STAMINA + 25
-    BRIEF_DESC_CHAR_CLASS = ('дерзкий воин ближнего боя. '
-                             'Сильный, выносливый и отважный')
+    """Класс персонажа - Warrior(Воитель)."""
+    RANGE_VALUE_ATTACK: Tuple[int, int] = (3, 5)
+    RANGE_VALUE_DEFENCE: Tuple[int, int] = (5, 10)
+    SPECIAL_SKILL: str = 'Выносливость'
+    SPECIAL_BUFF: int = DEFAULT_STAMINA + 25
+    BRIEF_DESC_CHAR_CLASS: str = (
+        'дерзкий воин ближнего боя. Сильный, выносливый и отважный'
+    )
 
 
 class Mage(Character):
-    RANGE_VALUE_ATTACK = (5, 10)
-    RANGE_VALUE_DEFENCE = (-2, 2)
-    SPECIAL_SKILL = 'Атака'
-    SPECIAL_BUFF = DEFAULT_ATTACK + 40
-    BRIEF_DESC_CHAR_CLASS = ('находчивый воин дальнего боя. '
-                             'Обладает высоким интеллектом')
+    """Класс персонажа - Mage(Маг)."""
+    RANGE_VALUE_ATTACK: Tuple[int, int] = (5, 10)
+    RANGE_VALUE_DEFENCE: Tuple[int, int] = (-2, 2)
+    SPECIAL_SKILL: str = 'Атака'
+    SPECIAL_BUFF: int = DEFAULT_ATTACK + 40
+    BRIEF_DESC_CHAR_CLASS: str = (
+        'находчивый воин дальнего боя. Обладает высоким интеллектом'
+    )
 
 
 class Healer(Character):
-    RANGE_VALUE_ATTACK = (-3, -1)
-    RANGE_VALUE_DEFENCE = (2, 5)
-    SPECIAL_SKILL = 'Защита'
-    SPECIAL_BUFF = DEFAULT_DEFENCE + 30
-    BRIEF_DESC_CHAR_CLASS = (' могущественный заклинатель. '
-                             'Черпает силы из природы, веры и духов')
+    """Класс персонажа - Healer(Лекарь)."""
+    RANGE_VALUE_ATTACK: Tuple[int, int] = (-3, -1)
+    RANGE_VALUE_DEFENCE: Tuple[int, int] = (2, 5)
+    SPECIAL_SKILL: str = 'Защита'
+    SPECIAL_BUFF: int = DEFAULT_DEFENCE + 30
+    BRIEF_DESC_CHAR_CLASS: str = (
+        'могущественный заклинатель. Черпает силы из природы, веры и духов'
+    )
 
 
 def start_training(char_class: Character) -> Optional[str]:
     """Тренировочная игра."""
     print('Потренируйся управлять своими навыками.')
-    print('Введи одну из команд: attack — чтобы атаковать противника, '
-          'defence — чтобы блокировать атаку противника или '
-          'special — чтобы использовать свою суперсилу.')
+    print(
+        'Введи одну из команд: attack — чтобы атаковать противника, '
+        'defence — чтобы блокировать атаку противника или '
+        'special — чтобы использовать свою суперсилу.'
+    )
     print('Если не хочешь тренироваться, введи команду skip.')
     commands = {
         'attack': char_class.attack,
@@ -81,11 +90,14 @@ def start_training(char_class: Character) -> Optional[str]:
     while cmd != 'skip':
         if cmd in commands:
             print(commands[cmd]())
+        else:
+            print('Убедитесь в правильности написания команды')
     return 'Тренировка окончена.'
 
 
 def choice_char_class(char_name: str) -> Character:
-    game_classes = {
+    """Функция выбора персонажа."""
+    game_classes: dict = {
         'warior': Warrior,
         'mage': Mage,
         'healer': Healer
@@ -93,20 +105,37 @@ def choice_char_class(char_name: str) -> Character:
     approve_choice: Optional[str] = None
 
     while approve_choice != 'y':
-        selected_class = input(
+        selected_class: str = input(
             'Введи название персонажа, '
             'за которого хочешь играть: '
             'Воитель — warrior, Маг — mage, Лекарь — healer: '
         ).lower()
         char_class: Character = game_classes[selected_class](char_name)
         print(char_class)
-        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
-                               'или любую другую кнопку, '
-                               'чтобы выбрать другого персонажа ').lower()
+        approve_choice = input(
+            'Нажми (Y), чтобы подтвердить выбор, '
+            'или любую другую кнопку, '
+            'чтобы выбрать другого персонажа '
+        ).lower()
     return char_class
 
 
 if __name__ == '__main__':
-    warrior = Warrior('Кодослав')
-    print(warrior)
-    print(warrior.attack())
+    from graphic_arts.start_game_banner import run_screensaver
+
+    def main():
+        run_screensaver()
+        print('Приветствую тебя, искатель приключений!')
+        print('Прежде чем начать игру...')
+        char_name = input('...назови себя: ')
+        print(
+            f'Здравствуй, {char_name}! '
+            f'Сейчас твоя выносливость — {DEFAULT_STAMINA}, '
+            f'атака — {DEFAULT_ATTACK} и защита — {DEFAULT_DEFENCE}.'
+        )
+        print('Ты можешь выбрать один из трёх путей силы:')
+        print('Воитель, Маг, Лекарь')
+        char_class = choice_char_class(char_name)
+        print(start_training(char_class))
+
+    main()
