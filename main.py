e5ca995da35a9e95309d34be885cc8d90925ce58
Новиков Максим <maxom.nowik@yandex.ru>
2022-11-16 00:16:32 +0400

    Fix readme.md.

--- main.py
+++ main.py
@@ -98,12 +98,12 @@ if __name__ == '__main__':
         run_screensaver()
         print('Приветствую тебя, искатель приключений!')
         print('Прежде чем начать игру...')
-        char_name: str = input('...назови себя: ')
+        char_name = input('...назови себя: ')
         print(f'Здравствуй, {char_name}! '
               'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
         print('Ты можешь выбрать один из трёх путей силы:')
         print('Воитель, Маг, Лекарь')
-        char_class: str = choice_char_class()
+        char_class = choice_char_class()
         print(start_training(char_name, char_class))
 
     main()
